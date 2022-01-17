0e16b0ff0254f63d0eb8d2e4b38b70f2c2a7cc12
ayoxdele-eng <70418626+ayoxdele-eng@users.noreply.github.com>
2022-01-17 07:50:42 +0100

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,5 @@
 public class HelloWorld {
   public static void main(String[] args) {
-    System.out.println("Hello Ayodele the champs and news!");
+    System.out.println("Hello Ayodele the champs and news Monday !");
   }
 }
